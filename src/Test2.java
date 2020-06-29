de7d7277269e90724d2e1ca922a1afd328c26510
deshyas <deshyas@CAM1XJ85S2.MFCGD.com>
2020-06-29 07:51:04 -0400

    commiting Test2 to BranchDeepa

---
+++ src/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="No";
+		if(hungry=="yes")
+			System.out.println("eat somthing");
+		else
+			System.out.println("Do Work");
+	}
+
+}
